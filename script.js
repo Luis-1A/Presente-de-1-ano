7ecbdb39c5a079695044da773ccae7257f113442
Luis Fernando santos <142061648+Luis-1A@users.noreply.github.com>
2026-01-15 09:39:22 -0300

    Implement star animation in script.js
    Added a function to create and animate stars in the scene.

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-// No JS. Thanks for visiting!
-// Thanks to @Aleks for robot divs
-// If you enjoy make sure to ♥ this codepen!
-// Not tested on mobile.
+function stars() {
+  let count = 50;
+  let scene = document.querySelector('.scene');
+  let i = 0;
+  while (i < count) {
+    let star = document.createElement('i');
+    let x = Math.floor(Math.random() * window.innerWidth);
+    let duration = Math.random() * 1;
+    let h = Math.random() * 100;
+
+    star.style.left = x + 'px';
+    star.style.width = 1 + 'px';
+    star.style.height = h + 'px';
+    star.style.animationDuration = duration + 's';
+
+    scene.appendChild(star);
+    i++;
+  }
+}
+
+stars();
